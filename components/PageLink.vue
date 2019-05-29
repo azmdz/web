8b0d141f5e850fa929546ff1869385e5c9fe0d12
dazi_gomi <dazigomi@gmail.com>
2019-05-30 01:10:04 +0900

    [add] Add PageLink components

---
+++ components/PageLink.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  ul.page__container
+    li
+      nuxt-link.link(to="/about") About
+    li
+      nuxt-link.link(to="/works") Works
+    li
+      nuxt-link.link(to="/products") Products
+    li
+      a.link(href="http://blog.dazigomi.com") Blog
+</template>
+
+<style lang="scss">
+  .page__container{
+    margin: 2rem auto;
+    display: flex;
+  }
+  .link{
+    position: relative;
+    color: #000;
+
+    &:after,&:before{
+      content: '';
+      position: absolute;
+      width: 100%;
+      height: 1px;
+      right: 0;
+      bottom: 0;
+      transition: all 0.18s ease-in;
+    }
+    &:before{
+      background-color: #f00;
+    }
+    &:after{
+      background-color: #000;
+    }
+    &:hover{
+      &:after{
+        width: 0;
+
+      }
+    }
+  }
+</style>
