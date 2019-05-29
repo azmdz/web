8c11285dd5b7ad6957e65d2e4a997f2f3e4bb7b6
dazi_gomi <dazigomi@gmail.com>
2019-05-30 01:09:42 +0900

    [mod] Mod css

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,10 +6,9 @@
 
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
+  font-family: Roboto,'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+     'Helvetica Neue', Arial, sans-serif;
+  font-size: 62.5%;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
@@ -31,25 +30,5 @@ html {
   color: #3b8070;
   text-decoration: none;
   padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
