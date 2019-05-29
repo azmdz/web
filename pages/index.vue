0fd6147e90e803a2bb8339e926b7fc775c67f184
dazi_gomi <dazigomi@gmail.com>
2019-05-30 01:05:27 +0900

    [mod] Mod index

--- pages/index.vue
+++ pages/index.vue
@@ -3,53 +3,32 @@
     div
       h1.title.
         dazigomi.com
-      ul.link__container
-        li
-          nuxt-link.link(to="/about") About
-        li
-          nuxt-link.link(to="/works") Works
-        li
-          nuxt-link.link(to="/products") Products
-        li
-          a.link(href="http://blog.dazigomi.com") Blog
+      PageLink
+      //- SocialLink
 </template>
 
 <script>
   import Logo from '~/components/Logo.vue'
+  import SocialLink from '~/components/SocialLink.vue'
+  import PageLink from '~/components/PageLink.vue'
 
   export default {
     components: {
-      Logo
+      Logo,
+      PageLink,
+      SocialLink
     }
   }
 </script>
 
 <style>
-  .container {
-    margin: 0 auto;
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    font-family: Roboto, 'Helvetica Neue', Arial, sans-serif;
-  }
+
 
   .title {
     display: block;
     font-size: 48px;
     font-weight: 300;
     color: #000;
-  }
-
-  .link__container {
-    margin: 2.5rem auto;
-  }
-  .link {
-    margin: 0 1rem;
-    font-size: 24px;
-    text-decoration: none;
-    font-weight: 300;
   }
 
 </style>
